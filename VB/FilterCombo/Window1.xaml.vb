97fb9cd470a30457e07faf8e9835e8e25235a2b1
DevExpressExampleBot <fakeEmail>
2022-12-17 03:24:58 +0400

    Source auto update [skip ci]

--- VB/FilterCombo/Window1.xaml.vb
+++ VB/FilterCombo/Window1.xaml.vb
@@ -1,100 +1,60 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.ComponentModel
-Imports System.Collections
 Imports DevExpress.Xpf.Editors.Settings
-Imports DevExpress.Xpf.Grid
-Imports DevExpress.Xpf.Editors
 Imports DevExpress.XtraPrinting
 
 Namespace FilterCombo
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	''' 
 
-	Partial Public Class Window1
-		Inherits Window
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    ''' 
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			cars = New BindingList(Of Car)()
-			colors = New BindingList(Of CustomColor)()
+        Public Sub New()
+            Me.InitializeComponent()
+            cars = New BindingList(Of Car)()
+            colors = New BindingList(Of CustomColor)()
+            cars.Add(New Car() With {.AvailableColor = 2, .Make = "Make1"})
+            cars.Add(New Car() With {.AvailableColor = 1, .Make = "Make2"})
+            cars.Add(New Car() With {.AvailableColor = 3, .Make = "Make1"})
+            Me.gridControl1.ItemsSource = cars
+            colors.Add(New CustomColor() With {.ID = 1, .Name = "Red"})
+            colors.Add(New CustomColor() With {.ID = 2, .Name = "Blue"})
+            colors.Add(New CustomColor() With {.ID = 3, .Name = "Pink"})
+            Dim combo As ComboBoxEditSettings = New ComboBoxEditSettings()
+            combo.ItemsSource = colors
+            combo.DisplayMember = "Name"
+            combo.ValueMember = "ID"
+            combo.IsTextEditable = False
+            Me.gridControl1.Columns("AvailableColor").EditSettings = combo
+        End Sub
 
-			cars.Add(New Car() With {
-				.AvailableColor = 2,
-				.Make = "Make1"
-			})
-			cars.Add(New Car() With {
-				.AvailableColor = 1,
-				.Make = "Make2"
-			})
-			cars.Add(New Car() With {
-				.AvailableColor = 3,
-				.Make = "Make1"
-			})
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        End Sub
 
-			gridControl1.ItemsSource = cars
+        Public cars As BindingList(Of Car)
 
-			colors.Add(New CustomColor() With {
-				.ID = 1,
-				.Name="Red"
-			})
-			colors.Add(New CustomColor() With {
-				.ID = 2,
-				.Name = "Blue"
-			})
-			colors.Add(New CustomColor() With {
-				.ID = 3,
-				.Name = "Pink"
-			})
+        Public colors As BindingList(Of CustomColor)
 
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.view.ExportToXls("1.xls", New XlsExportOptions() With {.TextExportMode = TextExportMode.Text})
+        End Sub
+    End Class
 
-			Dim combo As New ComboBoxEditSettings()
-			combo.ItemsSource = colors
-			combo.DisplayMember = "Name"
-			combo.ValueMember = "ID"
-			combo.IsTextEditable = False
-			gridControl1.Columns("AvailableColor").EditSettings = combo
+    Public Class Car
 
-		End Sub
+        Public Property AvailableColor As Integer
 
+        Public Property Make As String
+    End Class
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+    Public Class CustomColor
 
-		End Sub
+        Public Property ID As Integer
 
-		Public cars As BindingList(Of Car)
-		Public colors As BindingList(Of CustomColor)
-
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			view.ExportToXls("1.xls", New XlsExportOptions() With {.TextExportMode = TextExportMode.Text})
-		End Sub
-
-
-
-
-	End Class
-
-
-	Public Class Car
-		Public Property AvailableColor() As Integer
-		Public Property Make() As String
-	End Class
-
-	Public Class CustomColor
-		Public Property ID() As Integer
-		Public Property Name() As String
-	End Class
+        Public Property Name As String
+    End Class
 End Namespace
